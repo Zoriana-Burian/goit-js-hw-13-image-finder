92e5c99634ef6c55a830c987434e2f4015bdccab
Zoriana-Burian <ke7sha1991@gmail.com>
2020-11-11 14:03:30 +0200

    дописання функції onSearch, learImagesGallery, appendImagesGall

--- src/index.js
+++ src/index.js
@@ -1,7 +1,49 @@
 import getRefs from './refs';
 import './css/common.css';
 import NewsApiService from './apiService';
+import imagesTempl from './templates/images.hbs'
+import LoadMoreBtn from './load-more-btn.js'
 
 const refs = getRefs();
 
 const newsApiService = new NewsApiService();
+
+refs.form.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', fetchArticles);
+
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+  });
+
+  
+function onSearch(e){
+ e.preventDefault();
+ newsApiService.query = e.currentTarget.elements.query.value;
+
+ if (newsApiService.query === '') {
+    return alert('Введите название');
+  }
+
+  loadMoreBtn.show();
+  newsApiService.resetPage();
+  clearImagesGallery();
+  fetchArticles();
+}
+
+
+function fetchArticles() {
+    loadMoreBtn.disable();
+    newsApiService.fetchArticles().then(images => {
+        appendImagesGall(images);
+      loadMoreBtn.enable();
+    });
+  }
+
+function appendImagesGall(images) {
+    refs.gallery.insertAdjacentHTML('beforeend', imagesTempl(images));
+  }
+
+function clearImagesGallery() {
+    refs.gallery.innerHTML = '';
+  }
